224f8ecc5b0c86e92354f907c30ea188997437c4
Murtaza Tankiwala <115138620+murtazatankiwala456@users.noreply.github.com>
2024-04-30 12:51:45 +0530

    version 2 (read and create item in database)

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
+import mongoose from "mongoose";
+
 
 const app = express();
 const port = 3000;
@@ -7,22 +9,56 @@ const port = 3000;
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({extended:true}));
 
-const items =[]
-
-app.get("/", (req,res) =>{
-    const date = new Date();
-
-const options = { weekday: 'long', day: 'numeric', month: 'long' };
-const formattedDate = date.toLocaleDateString('en-US', options);
-
-
-    res.render("index.ejs", {items:items, newDate: formattedDate})
+mongoose.connect('mongodb://127.0.0.1:27017/todolistDB');
+const itemsSchema ={
+    name: String
+}
+const Item = mongoose.model("Item", itemsSchema)
+const item1 = new Item({
+    name:"cook Food"
+})
+const item2 = new Item({
+    name:"eat Food"
+})
+const item3 = new Item({
+    name:"serve Food"
 })
 
+const defaultItems = [item1,item2,item3]
+
+
+app.get("/", async (req, res) => {
+    try {
+      const date = new Date();
+      const options = { weekday: 'long', day: 'numeric', month: 'long' };
+      const formattedDate = date.toLocaleDateString('en-US', options);
+  
+      const foundItems = await Item.find({});
+
+    if(foundItems.length === 0){
+        Item.insertMany(defaultItems)
+        res.redirect("/")
+
+
+    }else{
+      
+        res.render("index.ejs", {newDate: formattedDate, items: foundItems,  });
+    }
+     
+    } catch (error) {
+      console.error('Error fetching items:', error);
+      res.status(500).send('Internal Server Error');
+    }
+  });
+
 app.post("/add", (req,res) =>{
-    const item =req.body.item
-    items.push(item);
+    const itemName =req.body.item
+    const item = new Item({
+        name: itemName
+    });
+    item.save()
     res.redirect("/")
+
     
 });
 
