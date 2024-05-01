3b71ca18c2fb64ccf75424e7e5c8688328760db2
Murtaza Tankiwala <115138620+murtazatankiwala456@users.noreply.github.com>
2024-05-01 09:27:40 +0530

    format code with prettier extension

--- index.js
+++ index.js
@@ -2,63 +2,54 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 
-
 const app = express();
 const port = 3000;
 
-app.use(express.static("public"))
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://127.0.0.1:27017/todolistDB');
-const itemsSchema ={
-    name: String
-}
-const Item = mongoose.model("Item", itemsSchema)
-
+mongoose.connect("mongodb://127.0.0.1:27017/todolistDB");
+const itemsSchema = {
+  name: String,
+};
+const Item = mongoose.model("Item", itemsSchema);
 
 app.get("/", async (req, res) => {
-    try {
-      
-      
-      const date = new Date();
-      const options = { weekday: 'long', day: 'numeric', month: 'long' };
-      const formattedDate = date.toLocaleDateString('en-US', options);
-  
-      const foundItems = await Item.find({});
-        res.render("index.ejs", {newDate: formattedDate, items: foundItems  });
-    }
-     
-     catch (error) {
-      console.error('Error fetching items:', error);
-      res.send('Internal Server Error');
-    }
-  });
+  try {
+    const date = new Date();
+    const options = { weekday: "long", day: "numeric", month: "long" };
+    const formattedDate = date.toLocaleDateString("en-US", options);
 
-app.post("/add", (req,res) =>{
-    const itemName =req.body.item
-    const item = new Item({
-        name: itemName
-    });
-    item.save()
-    res.redirect("/")
-
+    const foundItems = await Item.find({});
+    res.render("index.ejs", { newDate: formattedDate, items: foundItems });
+  } catch (error) {
+    console.error("Error fetching items:", error);
+    res.send("Internal Server Error");
+  }
 });
 
-app.post("/delete", async (req,res) =>{
-    try{
-    const itemId =req.body.checkbox;
+app.post("/add", (req, res) => {
+  const itemName = req.body.item;
+  const item = new Item({
+    name: itemName,
+  });
+  item.save();
+  res.redirect("/");
+});
 
-   await  Item.findByIdAndDelete(itemId)
-   console.log("deleted successfully!")
-   res.redirect("/")
-    }catch(error){
-        console.error("Not deleted", error);
-        res.send("Some error")
-    }
-})
+app.post("/delete", async (req, res) => {
+  try {
+    const itemId = req.body.checkbox;
 
+    await Item.findByIdAndDelete(itemId);
+    console.log("deleted successfully!");
+    res.redirect("/");
+  } catch (error) {
+    console.error("Not deleted", error);
+    res.send("Some error");
+  }
+});
 
-
-app.listen(port, (req,res) =>{
-    console.log(`listening the server on the port ${port} `)
-})
+app.listen(port, (req, res) => {
+  console.log(`listening the server on the port ${port} `);
+});
